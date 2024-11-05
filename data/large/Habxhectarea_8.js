a2213cd49b6ede8fee5e41c1788f032d8f299e58
kioskoexp <jomendoza@mikiosko.mx>
2024-11-05 12:17:41 -0600

    Delete data/large directory

--- data/large/Habxhectarea_8.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a5531e143faa14406477afd3549c5297b493b3fcb1eefda9f099c83f8bba4df9
-size 226130086
--- data/large/Tiendas_10.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:13a9f7376102a58a1e068e75290652663b46d1599e4bb4da80354bded3d688a5
-size 231251582
